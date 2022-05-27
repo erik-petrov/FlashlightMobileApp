eb1a4974fe6f8af5738ad6f239465119fed804cf
opilane <opilane@HP-CZC6297B9H>
2022-05-27 13:44:12 +0300

    Add project files.

---
+++ ProstoDajteMne3/ProstoDajteMne3/MainPage.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace ProstoDajteMne3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //InitializeComponent();
+            Label title = new Label
+            {
+                Text = "Добро пожаловать в мое приложение для фонарика!",
+                FontSize = 32
+            };
+            Button btn = new Button
+            {
+                Text = "Начать!"
+            };
+            btn.Clicked += Btn_Clicked;
+            Content = new StackLayout
+            {
+                Children = { title, btn }
+            };
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FlashlightPage());
+        }
+    }
+    public class FlashlightPage : ContentPage
+    {
+        
+        bool on, flick, sos;
+        Slider slider;
+        public FlashlightPage()
+        {
+            Button _switch = new Button
+            {
+                Text = "Включить фонарик!"
+            };
+            slider = new Slider
+            {
+                Maximum = 1000.0,
+                Minimum = 1.0,
+            };
+            Button enableSlider = new Button
+            {
+                Text = "Включить мерцание."
+            };
+            Button enableSos = new Button
+            {
+                Text = "Включить SOS сигнал."
+            };
+            enableSos.Clicked += EnableSos_Clicked;
+            enableSlider.Clicked += EnableSlider_Clicked;
+            _switch.Clicked += _switch_Clicked;
+            Content = new StackLayout
+            {
+                Children = { _switch, slider, enableSlider, enableSos }
+            };
+        }
+
+        private async void EnableSos_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (on)
+            {
+                sos = !sos;
+                if (sos)
+                {
+                    btn.Text = "Выключить SOS.";
+                    await SOS();
+                }
+                else
+                {
+                    btn.Text = "Включить SOS сигнал.";
+                }
+
+            }
+        }
+
+        private async void EnableSlider_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (on)
+            {
+                flick = !flick;
+                if(flick)
+                {
+                    btn.Text = "Выключить мерцание.";
+                    await FLICK();
+                }
+                else
+                {
+                    btn.Text = "Включить мерцание.";
+                }
+                    
+            }
+        }
+
+        private async Task FLICK()
+        {
+            while (flick && on)
+            {
+                await Flashlight.TurnOnAsync();
+                await Task.Delay((int)Math.Round(3000 / slider.Value, 0));
+                await Flashlight.TurnOffAsync();
+                await Task.Delay((int)Math.Round(3000 / slider.Value, 0));
+            };
+        }
+
+        private async Task SOS()
+        {
+            while (sos && on)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (!on || !sos)
+                        return;
+                    await Flashlight.TurnOnAsync();
+                    await Task.Delay(600);
+                    await Flashlight.TurnOffAsync();
+                    await Task.Delay(100);
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    if (!on || !sos)
+                        return;
+                    await Flashlight.TurnOnAsync();
+                    await Task.Delay(1200);
+                    await Flashlight.TurnOffAsync();
+                    await Task.Delay(300);
+                }
+            };
+        }
+
+        private async void _switch_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (!on)
+            {
+                btn.Text = "Выключить фонарик!";
+                await Flashlight.TurnOnAsync();
+            }
+            else
+            {
+                btn.Text = "Включить фонарик!";
+                await Flashlight.TurnOffAsync();
+            }
+                
+            flick = !flick;
+            on = !on;
+        }
+    }
+}
---
+++ ProstoDajteMne3/ProstoDajteMne3/MorseCode.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProstoDajteMne3
+{
+    public class MorseCode
+    {
+        private static Dictionary<char, string> _morseAlphabetDictionary;
+
+        static void Main()
+        {
+            InitializeDictionary();
+
+            Console.WriteLine("What did you want to say?");
+            string userInput = GetUserInput();
+            Console.WriteLine("Morse alphabet output is: " + Translate(userInput));
+
+            Console.WriteLine("[Press ANY KEY to exit]");
+            Console.ReadLine();
+        }
+
+        private static void InitializeDictionary()
+        {
+            _morseAlphabetDictionary = new Dictionary<char, string>()
+                                   {
+                                       {'a', ".-"},
+                                       {'b', "-..."},
+                                       {'c', "-.-."},
+                                       {'d', "-.."},
+                                       {'e', "."},
+                                       {'f', "..-."},
+                                       {'g', "--."},
+                                       {'h', "...."},
+                                       {'i', ".."},
+                                       {'j', ".---"},
+                                       {'k', "-.-"},
+                                       {'l', ".-.."},
+                                       {'m', "--"},
+                                       {'n', "-."},
+                                       {'o', "---"},
+                                       {'p', ".--."},
+                                       {'q', "--.-"},
+                                       {'r', ".-."},
+                                       {'s', "..."},
+                                       {'t', "-"},
+                                       {'u', "..-"},
+                                       {'v', "...-"},
+                                       {'w', ".--"},
+                                       {'x', "-..-"},
+                                       {'y', "-.--"},
+                                       {'z', "--.."},
+                                       {'0', "-----"},
+                                       {'1', ".----"},
+                                       {'2', "..---"},
+                                       {'3', "...--"},
+                                       {'4', "....-"},
+                                       {'5', "....."},
+                                       {'6', "-...."},
+                                       {'7', "--..."},
+                                       {'8', "---.."},
+                                       {'9', "----."}
+                                   };
+        }
+
+        private static string GetUserInput()
+        {
+            string input = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                input = input.ToLower();
+            }
+
+            return input;
+        }
+
+        private static string Translate(string input)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (char character in input)
+            {
+                if (_morseAlphabetDictionary.ContainsKey(character))
+                {
+                    stringBuilder.Append(_morseAlphabetDictionary[character] + " ");
+                }
+                else if (character == ' ')
+                {
+                    stringBuilder.Append("/ ");
+                }
+                else
+                {
+                    stringBuilder.Append(character + " ");
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
